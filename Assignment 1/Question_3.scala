5a9fd24b077e8934fc9b7cf482630fbfcb3b07eb
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-05-16 15:47:42 +0530

    Assignment 1 v3

--- Assignment 1/Question_3.scala
+++ Assignment 1/Question_3.scala
@@ -1,10 +1,10 @@
 object Question_3 extends App{
-  def TotalCost(books:Int):Double = {
+  def totalWholeSale (books:Int):Double = {
     if(books>50)
       24.95*0.6*books + 3*50 + (books-50)*0.75
     else
-      books*3
+      24.95*0.6*books + books*3
   }
 
-  print("The total wholesale cost for 60 copies is: "+ TotalCost(60))
+  print("The total wholesale cost for 60 copies is: "+ totalWholeSale(60))
 }
