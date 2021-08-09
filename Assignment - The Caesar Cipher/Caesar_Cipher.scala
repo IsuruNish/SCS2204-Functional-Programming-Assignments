9f888a0a2e6bb3b602439602bfcab9cb0e15b0a6
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-08-09 14:31:53 +0530

    The Caesar Cipher Assignment v1

---
+++ Assignment - The Caesar Cipher/Caesar_Cipher.scala
@@ -0,0 +1,12 @@
+object Caesar_Cipher extends App{
+  val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+
+  val encryption = (letter: Char, text:String, key:Int) => text((text.indexOf(letter.toUpper) + key) % text.length)
+
+  val decryption = (letter: Char, text:String, key:Int) => text((text.indexOf(letter.toUpper) - key + text.length) % text.length)
+
+  val CipherFunction = (f: (Char,String,Int) => Char, alph: String, key:Int , text: String) => text.map(f(_,alph, key))
+
+  println(CipherFunction(encryption,alphabet,17,"hello world i am isuru"))
+  println(CipherFunction(decryption,alphabet,17,CipherFunction(encryption,alphabet,17,"hello world i am isuru")))
+}
